31a7df157294380eba8e29a6fec99ea01ebdd47d
Yusuf Samsum <42996009+yusufsamsum@users.noreply.github.com>
2018-11-14 00:48:19 +0300

    constructor and setter method

--- src/GameManagement/GameMapper.java
+++ src/GameManagement/GameMapper.java
@@ -11,20 +11,30 @@ import java.util.*;
  * @author firatyildiz
  */
 public class GameMapper {
+    
+    // properties
     private Square[][] squares;
+    
+    // constructor
+    public GameMapper(int width, int height){
+        squares = new Square[width][height];
+        setSquares(width,height);
+    }
+    
+    // methods 
     private void setSquares(int width, int height){
         for(int i = 0; i < height; i++){
             int xCoor = i * 10;
             for(int j = 0; i < width; i++){
                 int yCoor = j * 10;
-                squares[i][j] = new Square(xCoor, yCoor, xCoor + 10, yCoor + 10);
+                squares[i][j].setXCoordinate(xCoor);
+                squares[i][j].setYCoordinate(yCoor);
+                squares[i][j].setHeight(10);
                 squares[i][j].setFilled(false);
             }
         }
     }
+    
+    
 
-    GameMapper(){
-        setSquares(13, 5);
-        setSquare(13, 5);
-    }
 }
