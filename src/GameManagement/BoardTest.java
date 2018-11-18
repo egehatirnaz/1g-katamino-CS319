94e0a900c534292093051f663787901d0c79f539
burakkorkmaz97 <burakkorkmaz.bilkent@gmail.com>
2018-11-18 23:18:37 +0300

    Update BoardTest.java

--- src/GameManagement/BoardTest.java
+++ src/GameManagement/BoardTest.java
@@ -5,16 +5,15 @@
  */
 package GameManagement;
 
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.*;
 import javafx.scene.Cursor;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.paint.Color;
-import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseDragEvent;
@@ -24,13 +23,15 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
-import jfxtras.labs.util.event.MouseControlUtil;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
@@ -43,7 +44,6 @@ public class BoardTest extends Application {
     final int startx= 200;
     final int starty= 190;
     double originX, originY;
-    private Scene window;
     @Override
     public void start(Stage primaryStage) {
         /*Button btn = new Button();
@@ -56,9 +56,19 @@ public class BoardTest extends Application {
             }
         });*/
 
-        GameMapper gm = new GameMapper( 5 );
+        GameMapper gm = new GameMapper( 1 );
         Group root = new Group();
+
+        /*Timeline time = new Timeline();
+        Text timer = new Text();
+        timer.setText(time.toString());
+        timer.setX(100);
+        timer.setY(100);
+        root.getChildren().add(timer);*/
+
         int start;
+
+
 
         Square[][] boardSquares = gm.getSquares();
         for( int i = 0; i < boardSquares.length; i++ )
@@ -82,11 +92,17 @@ public class BoardTest extends Application {
         Image green = new Image(Paths.get("src/GameManagement/media/green.png").toUri().toString());
         Image blue = new Image( Paths.get( "src/GameManagement/media/blue.png" ).toUri().toString() );
         Image yellow = new Image( Paths.get( "src/GameManagement/media/yellow.png" ).toUri().toString() );
+        Image stick = new Image(Paths.get("src/GameManagement/media/Stick.png").toUri().toString());
+        Image award = new Image(Paths.get("src/GameManagement/media/award.png").toUri().toString());
         //JPanel P = new JPanel();
         Scene scene = new Scene(root, 1000, 1000);
         ImageView img1 = new ImageView(green);
         ImageView img2 = new ImageView(blue);
         ImageView img3 = new ImageView(yellow);
+        ImageView stickView = new ImageView(stick);
+        ImageView awardView = new ImageView(award);
+        stickView.setX(485);
+        stickView.setY(130);
 
         ArrayList<ImageView> imageList =  new ArrayList<ImageView>();
 
@@ -104,6 +120,22 @@ public class BoardTest extends Application {
 
                         ImageView anImage = (ImageView)(t.getSource());
                         anImage.toFront();
+                        double angle = anImage.getRotate();
+
+                        if(t.getClickCount()%2==0) {
+                            //anImage.setRotate(40);
+                            //System.out.println(anImage.getRotate());
+                            SnapshotParameters param = new SnapshotParameters();
+                            param.setFill(Color.TRANSPARENT);
+                            param.setTransform(new Rotate(90,anImage.getImage().getHeight()/2,anImage.getImage().getWidth()/2));
+                            anImage.setImage(anImage.snapshot(param,null));
+
+                            //anImage.setRotate(anImage.getRotate()+90);
+                            // anImage.getTransforms().add(new Rotate(90,anImage.getX(),anImage.getY()));
+
+                            System.out.println("Rotation" + anImage.getX());
+                            System.out.println("Rotation" + anImage.getBoundsInParent());
+                        }
                     }
             );
 
@@ -117,28 +149,39 @@ public class BoardTest extends Application {
                         anImage.setX( offsetX + anImage.getX() );
                         anImage.setY( offsetY + anImage.getY() );
 
+
                         originX  = t.getSceneX();
                         originY = t.getSceneY();
                     }
 
             );
             imageList.get(i).setOnMouseReleased((t)-> {
-                for( int k = 0; k < boardSquares.length; k++ ) {
+                for( int k = 0; k < gm.getCurrentLevel(); k++ ) {
                     for (int j = 0; j < boardSquares[k].length; j++) {
                         Circle c = new Circle();
                         c.setCenterY((250) + (j) * 100);
                         c.setCenterX((250) + (k) * 100);
                         c.setRadius(3);
-                        c.setFill(Color.RED);
-                        root.getChildren().add(c);
-                        for( int noOfImage = 0; noOfImage < imageList.size(); noOfImage++ )
-                        {
+                        //c.setFill(Color.RED);
+                        boardSquares[k][j].setFilled(false);
+                        //root.getChildren().add(c);
+                        for( int noOfImage = 0; noOfImage < imageList.size(); noOfImage++ ) {
                             if (root.getChildren().get(root.getChildren().indexOf(imageList.get(noOfImage))).contains((250) + (k) * 100, (250) + (j) * 100)) {
-                                c.setFill(Color.GREEN);
+                                //c.setFill(Color.GREEN);
                                 boardSquares[k][j].setFilled(true);
                             }
                         }
                     }
+                }
+                if(gm.isLevelFinished()){
+                    System.out.println("Malatya");
+                    //awardView.setX(;
+                    awardView.setX(500);
+                    awardView.setY(200);
+                    root.getChildren().add(awardView);
+                    img1.setDisable(true);
+                    img2.setDisable(true);
+                    img3.setDisable(true);
                 }
             });
         }
@@ -219,6 +262,9 @@ public class BoardTest extends Application {
         root.getChildren().add(img2);
         root.getChildren().add(img3);
 
+        root.getChildren().add(stickView);
+
+
         /*for( int i = 0; i < boardSquares.length; i++ ) {
             for (int k = 0; k < boardSquares[i].length; k++) {
                 Circle c = new Circle();
@@ -241,14 +287,8 @@ public class BoardTest extends Application {
         //root.getChildren().get(root.getChildren().indexOf(img3)).con
         primaryStage.setTitle("Katamino");
         primaryStage.setScene(scene);
-        //primaryStage.show();
+        primaryStage.show();
 
-        window = scene;
-
-    }
-
-    public Scene returnScene(){
-        return window;
     }
 
     /**
