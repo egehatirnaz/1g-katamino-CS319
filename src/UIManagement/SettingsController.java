d97a3a0651a122292c3f5deaddbacb892d8e813e
Firat Yildiz <43797009+frtyldz@users.noreply.github.com>
2018-11-20 19:32:16 +0300

    Update SettingsController.java
    An import add for page changes

--- src/UIManagement/SettingsController.java
+++ src/UIManagement/SettingsController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
