7ec437125e52f5795174a4fa6dea95ee7b3c37d5
yusufsamsum <yusuf.samsum@ug.bilkent.edu.tr>
2018-12-17 18:07:56 +0300

    Direction class became more reasonable and flexible

--- src/GameManagement/Direction.java
+++ src/GameManagement/Direction.java
@@ -1,15 +1,76 @@
-
+package GameManagement;
 public class Direction {
-	int[] distance;
-	
-	int[] getDistance()
+	private int[] distance;
+	private int right;
+	private int left;
+	private int down;
+	private int up;
+
+
+	public Direction( int right, int down, int left, int up )
+	{
+		distance = new int[4];
+		this.right = right;
+		this.left = left;
+		this.down = down;
+		this.up = up;
+		setDistance( right, down, left, up);
+	}
+
+	public Direction(){}
+
+	public int[] getDistance()
 	{
 		return distance;
 	}
-	
-	void setDistance(int[] distance)
+
+	private void setDistance(int right, int down, int left, int up)
 	{
+		setRight( right );
+		setLeft( left );
+		setDown( down );
+		setUp( up );
+	}
+
+	public void setRight( int right )
+	{
+		this.right = right;
+		distance[ 0 ] = right;
+	}
+
+	public void setUp(int up) {
+		this.up = up;
+		distance[ 3 ] = up;
+	}
+
+	public void setDown(int down) {
+		this.down = down;
+		distance[ 1 ] = down;
+	}
+
+	public void setLeft( int left )
+	{
+		this.left = left;
+		distance[ 2 ] = left;
+	}
+
+	public void setDistance(int[] distance) {
 		this.distance = distance;
 	}
-	
+
+	public int getUp() {
+		return up;
+	}
+
+	public int getRight() {
+		return right;
+	}
+
+	public int getDown() {
+		return down;
+	}
+
+	public int getLeft() {
+		return left;
+	}
 }
