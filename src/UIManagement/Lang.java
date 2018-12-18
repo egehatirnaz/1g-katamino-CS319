85914fe4cebb5630e7c4e4cbf41e95a95cf36ccd
egehatirnaz <egehatirnaz@hotmail.com>
2018-12-18 23:24:12 +0300

    Added TextField ID to Settings Controller, Created Lang class for localizations.

---
+++ src/UIManagement/Lang.java
@@ -0,0 +1,82 @@
+package UIManagement;
+import java.lang.reflect.Array;
+
+/*
+ * Lang.java
+ *
+ * A class to hold English and Turkish strings and change languages within the game.
+ * Translation: Allahım dünyanın en büyük ameleliği bence.
+ *
+ * -Ege, Dec 18 2018
+ */
+
+
+enum LangType{
+    US,
+    TR
+}
+public class Lang {
+    LangType langtype = LangType.US;
+    boolean isTurkish = false;
+    public String[] strings;
+
+    Lang(LangType LT){
+        changeLocale(LT);
+    }
+
+    public void setTurkish(){
+        isTurkish = true;
+        langtype = LangType.TR;
+    }
+
+    public void setEnglish(){
+        isTurkish = false;
+        langtype = LangType.US;
+    }
+
+    public boolean getTurkish(){
+        return isTurkish;
+    }
+
+    public void changeLocale(LangType LT){
+        switch(LT) {
+            case TR:
+                setTurkish();
+                strings = new String[]{
+                        "OYUNU BAŞLAT",
+                        "SIRALAMA",
+                        "YAPIMCILAR",
+                        "OYUNDAN ÇIK",
+                        "MENÜYE GERİ DÖN",
+                        "AYARLAR",
+                        "Bir Nickname Gir",
+                        "Kaydet",
+                        "İptal",
+                        "Giriş Yap",
+                        "Müzik",
+                        "Süre",
+                        "Kullanıcı Adı"
+                };
+
+                break;
+            default:
+                setEnglish();
+                strings = new String[]{
+                        "START GAME",
+                        "LEADERBOARD",
+                        "CREDITS",
+                        "QUIT GAME",
+                        "RETURN TO MENU",
+                        "SETTINGS",
+                        "Enter Your Nickname",
+                        "Save",
+                        "Cancel",
+                        "Sign In",
+                        "Music",
+                        "Time",
+                        "Username"
+                };
+                break;
+        }
+    }
+}
