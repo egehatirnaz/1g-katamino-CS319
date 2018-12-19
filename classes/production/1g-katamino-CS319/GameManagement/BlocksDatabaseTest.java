02dddea5ec34a5de8ac3c4817fa1d1de563e58e2
mepsileli <43796772+mepsileli@users.noreply.github.com>
2018-12-19 22:12:01 +0300

    Add files via upload

---
+++ classes/production/1g-katamino-CS319/GameManagement/BlocksDatabaseTest.java
@@ -0,0 +1,36 @@
+public class BlocksDatabaseTest {
+
+	public static void main(String[] args) {
+		BlocksDatabase bd = new BlocksDatabase("13111997");
+		int[] a1 = {1,2,3,4};
+		int[] a2 = {5,6,7,8};
+		int[] a3 = {9,10,11,12};
+		int[] a4 = {13,14,15,16};
+		Direction d1 = new Direction();
+		d1.setDistance( a1 );	
+		Direction d2 = new Direction();
+		d2.setDistance( a2 );
+		Direction d3 = new Direction();
+		d3.setDistance( a3 );
+		Direction d4 = new Direction();
+		d4.setDistance( a4 );
+		Direction[] dir = {d1,d2,d3,d4};
+		Direction[] dir2 = {d1,d2};
+		Direction[] dir3 = {d1,d2, null,null};
+		bd.saveBlock("ali",dir);
+		bd.saveBlock("veli",dir2);
+		bd.saveBlock("mert",dir3);
+		
+		for (int i = 0; i < 4; i++)
+			for (int a = 0; a < 4; a++)
+		System.out.println(bd.getDirections("veli").get(i).getDistance()[a]);
+		//bd.clearDatabase();
+		bd.updateBlockInformation("mert", dir2);
+		bd.deleteBlock("ali");
+		System.out.println(bd.controlBlockName("mert"));
+		System.out.println(bd.controlBlockName("abdfas"));
+		//System.out.println(bd.getBlockNames());
+		bd.closeDatabase();
+	}
+
+}
--- classes/production/1g-katamino-CS319/GameManagement/SolutionDatabase.java
+++ classes/production/1g-katamino-CS319/GameManagement/SolutionDatabase.java
@@ -5,10 +5,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.io.*;
 
 public class SolutionDatabase {
-	
+
 	//VARIABLES
 	private String password;
 	String rvr = "\'";
@@ -16,43 +15,42 @@ public class SolutionDatabase {
 	PreparedStatement ps;
 	ResultSet rs;
 		
-	
+		
 	//CONSTRUCTOR
 	public SolutionDatabase(String password){
 		this.password = password;
-		
+			
 		createConnection();
 		try {
-			String creationCode = "CREATE TABLE IF NOT EXISTS blocks" + 
-					"( blockname character varying(10)," + 
-					"  firstvertex integer[]," + 
-					"  secondvertex integer[]," + 
-					"  thirdvertex integer[]," + 
-					"  fourthvertex integer[]," + 
-					"  CONSTRAINT persons_pkey PRIMARY KEY (blockname) )";
+			String creationCode = "CREATE TABLE IF NOT EXISTS solutions" + 
+						"( modename character varying(10)," + 
+						"  level integer," + 
+						"  solutionnumber integer," + 
+						"  solution character varying[]," +
+						"  UNIQUE(level, solutionnumber)," +
+						"  PRIMARY KEY (solution) )";
 			ps = c.prepareStatement(creationCode);
 			ps.execute();		
-	
-			if(getBlockNames().size() == 0 )
+		
+			/*if(getBlockNames().size() == 0 )
 			{
-			//createConnection();
 			String copyCode = " COPY blocks " + 
 					"FROM 'D:\\Users\\Mert\\eclipse-workspace\\Katamino\\blocksDatabase' DELIMITER ',' CSV HEADER;";
 			ps = c.prepareStatement(copyCode);
 			ps.execute();				
-			} 
-			
+			} */
+				
 		}catch (SQLException e) {
 				e.printStackTrace();
 		}
 
 	}
 	
-	//HELPER METHODS
-	String directionToString(Direction dir)
+	//HELPER 
+	String arrayToString(String[] array)
 	{
-		if( dir != null) {
-		String str = Arrays.toString(dir.getDistance()).replaceAll("\\s+","");
+		if( array != null) {
+		String str = Arrays.toString(array).replaceAll("\\s+","");
 		str = str.replaceAll("\\[", "{");
 		str = str.replaceAll("]", "}");
 		return str;
@@ -61,30 +59,25 @@ public class SolutionDatabase {
 			return "{}";
 	}
 	
-	Direction stringToDirection(String str)
+	ArrayList<String> stringToArrayList(String str)
 	{
 		int fComma=0;
 		int sComma=0;
-		int arrayIndex=0;
-		int[] array = new int[4];
-		Direction dir = new Direction();
-		if (!str.equals("{}")) {
+		ArrayList<String> temp = new ArrayList<String>();
+		if (!str.equals("")) {
 		for(int a = 0; a < str.length(); a++)
 		{			
 			if(str.charAt(a) == ',')
 			{
 				sComma = a;
-				array[arrayIndex++] = Integer.parseInt( str.substring(fComma+1,sComma) );
+				temp.add( str.substring(fComma+1,sComma) );
 				fComma = sComma;
 			}
 		}
-		array[3] = Integer.parseInt(str.substring(fComma+1,str.length()-1));
+		temp.add( str.substring( fComma+1,str.length()-1) );
 		}
-		
-		dir.setDistance(array);	
-		return dir;
+		return temp;
 	}
-	
 	
 	//DATABASE METHODS
 	void createConnection()
@@ -117,7 +110,6 @@ public class SolutionDatabase {
 			}
 	}
 	
-	
 	void closeDatabase()
 	{
 		try {
@@ -129,168 +121,90 @@ public class SolutionDatabase {
 	}
 	
 	void deleteDatabase()
-	{
-		//createConnection();
-		
+	{	
 		try {
-			String deletionCode = "DROP TABLE [ IF EXISTS ] blocks";
+			String deletionCode = "DROP TABLE [ IF EXISTS ] solutions";
 			ps = c.prepareStatement(deletionCode);
 			ps.execute();		
 
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-		//closeDatabase();
 	}
 	
 	void clearDatabase()
-	{
-		//createConnection();
-		
+	{	
 		try {
-			String clearCode = "DELETE FROM blocks";
+			String clearCode = "DELETE FROM solutions";
 			ps = c.prepareStatement(clearCode);
 			ps.execute();		
 
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-		
-		//closeDatabase();
 	}
 	
-	void updateBlockInformation(String blockname , Direction[] directions)
+	void addSolution (String modeName, int level, int solutionNumber ,String[] solution)
 	{
-		String updateCode = "UPDATE blocks SET firstvertex = " + rvr + directionToString(directions[0]) + rvr +
-							", secondvertex = " + rvr + directionToString(directions[1]) + rvr +
-							", thirdvertex = " + rvr + directionToString(directions[2]) + rvr +
-							", fourthvertex = " + rvr + directionToString(directions[3]) + rvr +
-							" WHERE blockname = '" + blockname + "';";
-		System.out.println(updateCode);
-		if(!controlBlockName(blockname)) {
-		//createConnection();	
 		try {
-			ps = c.prepareStatement(updateCode);
-			ps.execute();		
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		//closeDatabase();
-		}
-	}
-	ArrayList<String> getBlockNames()
-	{
-		ArrayList<String> savedBlockNames = new ArrayList<String>();
-		//createConnection();
-		try {
-		String blocknameCode = "SELECT blockname FROM blocks";
-		ps = c.prepareStatement(blocknameCode);
-		rs = ps.executeQuery();		
-		while(rs.next())
-		savedBlockNames.add(rs.getString("blockname"));		
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return savedBlockNames;	
-	}
-	
-	//Return false if there is a block with this name.
-	boolean controlBlockName(String blockName)
-	{
-		for(int a = 0; a < getBlockNames().size(); a++)
-			if (getBlockNames().get(a).equals(blockName))
-				return false;
-		return true;
-	}
-	
-	void saveBlock(String blockName, Direction[] directions)
-	{
-		directionToString(directions[0]);
-
-		if(controlBlockName(blockName)) {
-			//createConnection();
-		try {
-			String insertionCode = "INSERT INTO blocks (blockname,firstvertex,secondvertex,thirdvertex,fourthvertex)"
-					+ " VALUES (" + rvr + blockName + rvr;
-			for(int i = 0; i < directions.length; i++)
-					insertionCode = insertionCode + "," + rvr + directionToString(directions[i]) + rvr;
+			String insertionCode = "INSERT INTO solutions (modename, level, solutionnumber, solution)"
+					+ " VALUES (" + rvr + modeName + rvr + "," + rvr + String.valueOf(level) + rvr + "," + rvr + 
+					String.valueOf(solutionNumber) + rvr + "," + rvr + arrayToString(solution) + rvr + ")";
 			
-			for(int i = directions.length; i < 4; i++)
-				insertionCode = insertionCode + "," + rvr + "{}" + rvr;
-			
-			insertionCode = insertionCode + ")";
-					
 			ps = c.prepareStatement(insertionCode);
 			ps.execute();	
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			//closeDatabase();
-		}
 	}
 	
-	ArrayList<Direction> getDirections(String blockName){
-		ArrayList<Direction> directions = new ArrayList<Direction>();
-
-			if( !controlBlockName(blockName) ) {	
-				//createConnection();
-			String fv = "SELECT firstvertex FROM blocks WHERE blockname = " + "\'" + blockName + "\'";
-			String sv = "SELECT secondvertex FROM blocks WHERE blockname = " + "\'" + blockName + "\'";
-			String tv = "SELECT thirdvertex FROM blocks WHERE blockname = " + "\'" + blockName + "\'";
-			String fov = "SELECT fourthvertex FROM blocks WHERE blockname = " + "\'" + blockName + "\'";
-
-			
-			try {
-				ps = c.prepareStatement(fv);
-				ResultSet rs = ps.executeQuery();		
-				while(rs.next()) {
-					if(!rs.getString("firstvertex").equals(""))
-						directions.add( stringToDirection( rs.getString("firstvertex") ) );
-				}
-				
-				ps = c.prepareStatement(sv);
-				rs = ps.executeQuery();		
-				while(rs.next()) {
-					if(!rs.getString("secondvertex").equals(""))
-						directions.add( stringToDirection( rs.getString("secondvertex") ) );
-				}
-				
-				ps = c.prepareStatement(tv);
-				rs = ps.executeQuery();		
-				while(rs.next())
-				{
-					if(!rs.getString("thirdvertex").equals(""))
-						directions.add( stringToDirection( rs.getString("thirdvertex") ) );
-				}
-				
-				ps = c.prepareStatement(fov);
-				rs = ps.executeQuery();		
-				while(rs.next())
-				{
-					if(!rs.getString("fourthvertex").equals(""))
-					directions.add( stringToDirection( rs.getString("fourthvertex") ) );			
-				}
-				
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}	
-			}
-					
-        return directions;
-	}
-	
-	void deleteBlock(String blockName)
+	ArrayList<String> getSolution(String mode, int level, int solutionNumber)
 	{
-		if (!controlBlockName(blockName) ) {
-			try {
-				String blocknameCode = "DELETE FROM blocks WHERE blockname = '" + blockName + "';";
-				ps = c.prepareStatement(blocknameCode);
-				ps.execute();		
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
+		String temp = "";
+		try {
+			String solutionCode = "SELECT solution FROM solutions WHERE modename = '" + mode + "' AND level = " + level + 
+					" AND solutionnumber = " + solutionNumber;
+			System.out.println(solutionCode);
+			ps = c.prepareStatement(solutionCode);
+			rs = ps.executeQuery();		
+		if(temp.equals("")) {
+		while(rs.next())
+				temp = (rs.getString("solution"));
 		}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return stringToArrayList(temp);	
+	}
+	
+	void deleteSolution(String modeName, int level, int solutionNumber)
+	{
+		try {
+			String deletionCode = "DELETE FROM solutions WHERE modename = '" + modeName + "' AND level = " + String.valueOf(level) +
+									" AND solutionnumber = " + String.valueOf(solutionNumber) + ";";
+			ps = c.prepareStatement(deletionCode);
+			ps.execute();		
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+	}
+	
+	int getSolutionNumber(String modeName, int level)
+	{
+		int number = 0;
+		try {
+			String solutionNumberCode = "SELECT COUNT(*) FROM solutions WHERE modename = '" + modeName +
+					"' , level = '" + level;
+			ps = c.prepareStatement(solutionNumberCode);
+			rs = ps.executeQuery();
+			while(rs.next())
+				number = rs.getInt("count");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		return number;
 	}
 	
 }
