974c22151e150eaec7273a033d7589969fbb802a
mepsileli <43796772+mepsileli@users.noreply.github.com>
2018-12-21 01:31:04 +0300

    Update SolutionDatabaseTest.java

--- classes/production/1g-katamino-CS319/GameManagement/SolutionDatabaseTest.java
+++ classes/production/1g-katamino-CS319/GameManagement/SolutionDatabaseTest.java
@@ -4,6 +4,11 @@ public class SolutionDatabaseTest {
 
 	public static void main(String[] args) {
 		SolutionDatabase sd = new SolutionDatabase("13111997");
+		String [] dyn1 = {"n6", "d1", "d2"};
+		String [] dyn2= {"n9", "n7", "d1", "d2", "d3"};
+
+		sd.addSolution("DynamicMode", 1, 1, dyn1 );
+		sd.addSolution("DynamicMode", 2, 1, dyn2 );
 		sd.closeDatabase();
 	}
 
