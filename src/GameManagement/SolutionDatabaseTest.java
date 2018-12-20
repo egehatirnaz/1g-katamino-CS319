cf6e54cb1c7b61c62d6279baab1157ec686949b0
mepsileli <43796772+mepsileli@users.noreply.github.com>
2018-12-21 01:23:04 +0300

    Update SolutionDatabaseTest.java

--- src/GameManagement/SolutionDatabaseTest.java
+++ src/GameManagement/SolutionDatabaseTest.java
@@ -11,12 +11,17 @@ public class SolutionDatabaseTest {
         String [] level6 = {"n1", "n4", "n5", "n12", "n3", "n7"};
         String [] level7 = {"n1", "n4", "n5", "n12", "n3", "n7", "n6"};
         String [] level8 = {"n1", "n4", "n5", "n12", "n3", "n7", "n6", "n8"};
+        String [] dyn1 = {"n6", "d1", "d2"};
+        String [] dyn2= {"n9", "n7", "d1", "d2", "d3"};
 
         sd.addSolution("NormalMode", 4, 1,level4 );
         sd.addSolution("NormalMode", 5, 1,level5 );
         sd.addSolution("NormalMode", 6, 1,level6 );
         sd.addSolution("NormalMode", 7, 1,level7 );
         sd.addSolution("NormalMode", 8, 1,level8 );
+
+        sd.addSolution("DynamicMode", 1, 1, dyn1 );
+        sd.addSolution("DynamicMode", 2, 1, dyn2 );
     }
 
 }
