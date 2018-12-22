1051403833e46ca805b3476403145ab3f711c3d2
mepsileli <43796772+mepsileli@users.noreply.github.com>
2018-12-22 23:27:00 +0300

    Add files via upload

--- src/GameManagement/SolutionDatabase.java
+++ src/GameManagement/SolutionDatabase.java
@@ -1,4 +1,5 @@
 package  GameManagement;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -33,19 +34,10 @@ public class SolutionDatabase {
 			ps = c.prepareStatement(creationCode);
 			ps.execute();
 
-			if(getSolution("ChallangeMode", 4,1).size() == 0 )
-			{
-				String copyCode = " COPY solutions " +
-						" FROM '" + System.getProperty("user.dir") +
-						"/src/GameManagement/databases/solutionDatabase' DELIMITER ',' CSV HEADER;";
-				ps = c.prepareStatement(copyCode);
-				ps.execute();
-			}
-				
 		}catch (SQLException e) {
 				e.printStackTrace();
 		}
-
+		init();
 	}
 	
 	//HELPER 
@@ -206,6 +198,86 @@ public class SolutionDatabase {
 				e.printStackTrace();
 			}
 		return number;
+	}
+
+	public void init(){
+		if(getSolution("ChallangeMode", 4,1).size() == 0 )
+		{
+			String [] level4 = {"n1", "n4", "n5", "n12"};
+			String [] level5 = {"n1", "n4", "n5", "n12", "n3"};
+			String [] level6 = {"n1", "n4", "n5", "n12", "n3", "n7"};
+			String [] level7 = {"n1", "n4", "n5", "n12", "n3", "n7", "n6"};
+			String [] level8 = {"n1", "n4", "n5", "n12", "n3", "n7", "n6", "n8"};
+			String [] level9 = {"n1", "n4", "n5", "n12", "n3", "n7", "n6", "n8", "n11"};
+			String [] level10 = {"n1", "n4", "n5", "n12", "n3", "n7", "n6", "n8", "n11", "n10"};
+			String [] level11 = {"n1", "n4", "n5", "n12", "n3", "n7", "n6", "n8", "n11", "n10", "n2"};
+
+			addSolution("ChallangeMode", 4, 1,level4 );
+			addSolution("ChallangeMode", 5, 1,level5 );
+			addSolution("ChallangeMode", 6, 1,level6 );
+			addSolution("ChallangeMode", 7, 1,level7 );
+			addSolution("ChallangeMode", 8, 1,level8 );
+			addSolution("ChallangeMode", 9, 1,level9 );
+			addSolution("ChallangeMode", 10, 1,level10 );
+			addSolution("ChallangeMode", 11, 1,level11 );
+
+			String [] level4_2 = {"n1", "n9", "n3", "n11"};
+			String [] level5_2 = {"n1", "n9", "n3", "n11", "n5"};
+			String [] level6_2 = {"n1", "n9", "n3", "n11", "n5", "n12"};
+			String [] level7_2 = {"n1", "n9", "n3", "n11", "n5", "n12", "n8"};
+			String [] level8_2 = {"n1", "n9", "n3", "n11", "n5", "n12", "n8", "n6"};
+			String [] level9_2 = {"n1", "n9", "n3", "n11", "n5", "n12", "n8", "n6", "n10"};
+			String [] level10_2 = {"n1", "n9", "n3", "n11", "n5", "n12", "n8", "n6", "n10", "n4"};
+			String [] level11_2 = {"n1", "n9", "n3", "n11", "n5", "n12", "n8", "n6", "n10", "n4", "n7"};
+
+			addSolution("ChallangeMode", 4, 2,level4_2 );
+			addSolution("ChallangeMode", 5, 2,level5_2 );
+			addSolution("ChallangeMode", 6, 2,level6_2 );
+			addSolution("ChallangeMode", 7, 2,level7_2 );
+			addSolution("ChallangeMode", 8, 2,level8_2 );
+			addSolution("ChallangeMode", 9, 2,level9_2 );
+			addSolution("ChallangeMode", 10, 2,level10_2 );
+			addSolution("ChallangeMode", 11, 2,level11_2 );
+
+
+			String [] level4_3 = {"n1", "n9", "n12", "n6"};
+			String [] level5_3 = {"n1", "n9", "n12", "n6", "n11"};
+			String [] level6_3 = {"n1", "n9", "n12", "n6", "n11", "n2"};
+			String [] level7_3 = {"n1", "n9", "n12", "n6", "n11", "n2", "n4"};
+			String [] level8_3 = {"n1", "n9", "n12", "n6", "n11", "n2", "n4", "n3"};
+			String [] level9_3 = {"n1", "n9", "n12", "n6", "n11", "n2", "n4", "n3", "n5"};
+			String [] level10_3 = {"n1", "n9", "n12", "n6", "n11", "n2", "n4", "n3", "n5", "n7"};
+			String [] level11_3 = {"n1", "n9", "n12", "n6", "n11", "n2", "n4", "n3", "n5", "n7", "n10"};
+
+			addSolution("ChallangeMode", 4, 3,level4_3 );
+			addSolution("ChallangeMode", 5, 3,level5_3 );
+			addSolution("ChallangeMode", 6, 3,level6_3 );
+			addSolution("ChallangeMode", 7, 3,level7_3 );
+			addSolution("ChallangeMode", 8, 3,level8_3 );
+			addSolution("ChallangeMode", 9, 3,level9_3 );
+			addSolution("ChallangeMode", 10, 3,level10_3 );
+			addSolution("ChallangeMode", 11, 3,level11_3 );
+
+			String [] level3_4 = {"n1", "n9" , "n7"};
+			String [] level4_4 = {"n1", "n9" , "n7", "n6"};
+			String [] level5_4 = {"n1", "n9" , "n7", "n6", "n2"};
+			String [] level6_4 = {"n1", "n9" , "n7", "n6", "n2", "n5"};
+			String [] level7_4 = {"n1", "n9" , "n7", "n6", "n2", "n5", "n12"};
+			String [] level8_4 = {"n1", "n9" , "n7", "n6", "n2", "n5", "n12", "n4"};
+			addSolution("NormalMode", 3, 1,level3_4 );
+			addSolution("NormalMode", 4, 1,level4_4 );
+			addSolution("NormalMode", 5, 1,level5_4 );
+			addSolution("NormalMode", 6, 1,level6_4 );
+			addSolution("NormalMode", 7, 1,level7_4 );
+			addSolution("NormalMode", 8, 1,level8_4 );
+
+			String [] dyn1 = {"n6", "d1", "d2"};
+			String [] dyn2 = {"n9", "n7", "d1", "d2", "d3"};
+			String [] dyn3 = { "d1", "d2", "d4", "d5", "n4", "n6", "n9" };
+		    addSolution("DynamicMode", 1, 1, dyn1 );
+		    addSolution("DynamicMode", 2, 1, dyn2 );
+			addSolution("DynamicMode", 3,1, dyn3 );
+		}
 	}
 	
 }
