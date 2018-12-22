a94223edde9eca4fb6603c2e625f8f400f635d02
firatyildiz <frtyldz9708@gmail.com>
2018-12-22 09:44:45 +0300

    Time has been fixed

--- src/GameManagement/NormalModeMapper.java
+++ src/GameManagement/NormalModeMapper.java
@@ -10,6 +10,7 @@ public class NormalModeMapper extends CommonMapper {
 
     private ArrayList<ImageView> imageList;
     private SolutionDatabase solutionDatabase;
+
     public  NormalModeMapper(String password){
         solutionDatabase = new SolutionDatabase(password);
         imageList = new ArrayList<>();
@@ -37,16 +38,6 @@ public class NormalModeMapper extends CommonMapper {
             imageList.add(blockView);
         }
 
-        /*
-        Image green = new Image( Paths.get(str).toUri().toString());
-        Image blue = new Image( Paths.get( "src/GameManagement/media/n2.png" ).toUri().toString() );
-        Image yellow = new Image( Paths.get( "src/GameManagement/media/n3.png" ).toUri().toString() );
-        ImageView img1 = new ImageView( green );
-        ImageView img2 = new ImageView( blue );
-        ImageView img3 = new ImageView( yellow );
-        imageList.add( img1 );
-        imageList.add( img2 );
-        imageList.add( img3 );*/
     }
 
 }
